558f55ced2902cf22e7401e975cb10e237e09edd
never-w <1737666820@qq.com>
2024-02-18 16:45:56 +0800

    feat: init

---
+++ src/lexer.ts
@@ -0,0 +1,14 @@
+export enum TokenType {
+  // Literal Types
+  Number,
+  Identifier,
+
+  // Keywords
+  Let,
+
+  // Grouping * Operators
+  BinaryOperator,
+  Equals,
+  OpenParen,
+  CloseParen,
+}
