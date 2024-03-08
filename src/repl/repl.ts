42856908dfb34068d9f2ba4c9fe6027a967295aa
yinqiao <1737666820@qq.com>
2024-03-08 22:00:37 +0800

    feat: 实现parse阶段和repl

--- src/repl/repl.ts
+++ src/repl/repl.ts
@@ -1,6 +1,20 @@
 import readline from 'readline'
 import { Lexer } from '../lexer/lexer'
 import { TokenTypes } from '../token/token'
+import { Parser } from '../parser/parser'
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 const repl = readline.createInterface({
   input: process.stdin,
@@ -12,13 +26,13 @@ repl.prompt()
 
 repl.on('line', (input) => {
   const lexer = Lexer.newLexer(input)
-
-  while (true) {
-    const token = lexer.nextToken()
-    console.log(token)
-    if (token.type === TokenTypes.EOF) break
+  const parser = Parser.newParser(lexer)
+  const program = parser.parseProgram()
+  if (parser.getErrors().length) {
+    printParseErrors(parser.getErrors())
   }
 
+  console.log(program.string())
   repl.prompt()
 })
 
@@ -26,3 +40,12 @@ repl.on('close', () => {
   console.log('Have a nice day!')
   process.exit(0)
 })
+
+function printParseErrors(errors: string[]) {
+  console.error(MONKEY_FACE)
+  console.error('Woops! we ran into some monkey business here!')
+  console.error(' parser errors:')
+  for (const err of errors) {
+    console.log(`\t ${err}`)
+  }
+}
